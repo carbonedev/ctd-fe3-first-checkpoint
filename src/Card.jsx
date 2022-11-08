8965a05ed20aa07ff954f4e3f3121536ba86dcf8
carbonedev <arboness@gmail.com>
2022-11-07 22:42:57 -0300

    estrutura

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,16 @@
+
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
 
-export function Card () {
+export function Card (props) {
   return (
-    <div>
 
-    </div>
+    <main className="card-color">
+      <li>
+        <div className="card-body">
+          <h1>{props.colorData.name}</h1>
+          <span>{props.colorData.hex}</span>
+        </div>
+      </li>
+    </main>
   )
 }
